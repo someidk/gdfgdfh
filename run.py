ec38a23dd3707b79a88a8a92edadb53c0a426726
someidk <159027402+someidk@users.noreply.github.com>
2024-02-05 15:33:31 +0100

    Update run.py

--- run.py
+++ run.py
@@ -99,8 +99,10 @@ def index():
     return 'Alive'
     
 def run():
-    app.run(host=0.0.0.0, port=8080)
+    app.run(host='0.0.0.0', port=8080)
     
 def keep_alive():
     t = Thread(target=run)
     t.start()
+
+keep_alive()
